7ffe2f5af1737bf91145c3742f5281051121544f
Jeremy Levallois <jeremy.levallois@liris.cnrs.fr>
2014-07-04 20:16:26 +0200

    maj VolReader/Writer in order to use the center from vol file

--- src/DGtal/io/writers/VolWriter.h
+++ src/DGtal/io/writers/VolWriter.h
@@ -70,6 +70,7 @@ namespace DGtal
     // ----------------------- Standard services ------------------------------
     typedef TImage Image;
     typedef typename TImage::Value Value;
+    typedef typename TImage::Domain::Point Point;
     typedef TFunctor Functor;
     
     BOOST_CONCEPT_ASSERT((  CUnaryFunctor<TFunctor, Value, unsigned char> )) ;    
@@ -85,6 +86,9 @@ namespace DGtal
      */
     static bool exportVol(const std::string & filename, const Image &aImage, 
 			  const Functor & aFunctor = Functor()) throw(DGtal::IOException);
+
+    static bool exportVol(const std::string & filename, const Image &aImage, const Point &aCenter,
+        const Functor & aFunctor = Functor()) throw(DGtal::IOException);
   };
 }//namespace
 
